2588a73d39ea96373daa17b2aa0abb1042cce072
z-gora <domdlapsa@gmail.com>
2016-10-27 12:08:46 +0200

    Added commentary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 
+
 class Player():
     def __init__(self, w, h):
         self.x = 0
@@ -12,9 +13,11 @@ class Player():
     def update(self, x, y):
         self.x = x
         self.y = y
+        # We need to have a list off all the player's corners for the projection process
         self.vertices = [[self.x, self.y], [self.x+self.height, self.y], [self.x+self.height, self.y + self.width], [self.x, self.y+self.width]]
 
     def project(self, normal):
+        # Comments to the projection process are placed in the Polygon class
         projected = []
         for vect in self.vertices:
             dp = vect[0] * normal[0] + vect[1] * normal[1]
@@ -31,20 +34,33 @@ class Polygon():
         self.vertices = vert
 
     def collide(self, player):
+        # We check whether the player is colliding with the polygon
         collided = 0
+        checked = []
+        # We check every face
         for i in range(len(self.vertices)):
-            nexti = (i+1)%len(self.vertices)
+            nexti = (i+1) % len(self.vertices)
+            # Using vector substraction we obtain the vector representing the face...
             vector = [self.vertices[nexti][0]-self.vertices[i][0], self.vertices[nexti][1]-self.vertices[i][1]]
+            # ...its lenght...
             len_v = math.sqrt(vector[0]**2+vector[1]**2)
+            # ... and convert it to a unit vector
             unit_v = [vector[0]/len_v, vector[1]/len_v]
+            # Then we calculate the vector perpendicular to it, which represents the normal axis
             normal = [-unit_v[1], unit_v[0]]
+            checked.append(normal)
+            # We project sprites onto the axis
             me_p = self.project(normal)
             player_p = player.project(normal)
+            # And check for overlap
             if (me_p[1] < player_p[0]) or (me_p[0]>player_p[1]):
                 collided = 0
+                # Thanks to the rules of the Separating Axes Theorem
+                # we can stop checking when there is no overlap on at least one of the axes
                 break
             else:
                 collided = 1
+        # We return the correct screen colour
         if collided:
             return (0,255,0)
         else:
@@ -52,11 +68,13 @@ class Polygon():
 
     def project(self, normal):
         projected = []
+        # We project every vortex onto the normal vector
         for vect in self.vertices:
             dp = vect[0]*normal[0] + vect[1]*normal[1]
             projected_v = [normal[0]*dp, normal[1]*dp]
             projected_l = math.sqrt(projected_v[0]**2 + projected_v[1]**2)
             projected.append(projected_l)
+        # We return the min and max vector length - the boundaries of the projection
         return [min(projected), max(projected)]
 
     def draw(self, screen):
